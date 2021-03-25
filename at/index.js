3a835b33165381af8d7b9cd2006fea94ef65c369
Patrick Tunezerwane <tunezepatrick@gmail.com>
2021-03-25 23:14:15 +0200

    the at() method

---
+++ at/index.js
@@ -0,0 +1,6 @@
+const sentence = 'This is the sentence for testing';
+const index = 5;
+// const minusIndex = -4;
+
+console.log(`The char at ${index} is ${sentence.charAt(index)}`)
+// console.log(`The char at ${index} is ${sentence.charAt(minusIndex)}`)
