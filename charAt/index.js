aff259bf87db7a99e8c889a9843cd36d0b5c44a7
Patrick Tunezerwane <tunezepatrick@gmail.com>
2021-03-25 23:49:55 +0200

    display the last char

--- charAt/index.js
+++ charAt/index.js
@@ -13,6 +13,8 @@ const invoice = 'invoice05'
 
 const returnLast = (str) => {
     console.log(str.charAt(2))
+    // The last char
+    console.log(str.charAt(str.length - 1))
     return str.charAt(1)
 }
 
